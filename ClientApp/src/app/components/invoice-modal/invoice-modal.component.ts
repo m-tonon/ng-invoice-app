75cce3c0ae17b7a846186eb0fb2948bd0ca57850
m-tonon <math.tonon@gmail.com>
2023-07-05 05:52:05 -0300

    feat: implement reactive form in the client side

--- ClientApp/src/app/components/invoice-modal/invoice-modal.component.ts
+++ ClientApp/src/app/components/invoice-modal/invoice-modal.component.ts
@@ -1,25 +1,50 @@
 import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './invoice-modal.component.html',
-  styleUrls: ['./invoice-modal.component.scss']
+  styleUrls: ['./invoice-modal.component.scss'],
 })
 export class InvoiceModalComponent {
   @Output() invoiceOpen: EventEmitter<boolean> = new EventEmitter<boolean>();
   isInvoiceOpen: boolean = false;
+  isFormEdited: boolean = false;
+  form = this.fb.group({
+    billerStreetAddress: [''],
+    billerCity: [''],
+    billerZipCode: [''],
+    billerCountry: [''],
+    clientName: [''],
+    clientEmail: [''],
+    clientStreetAddress: [''],
+    clientCity: [''],
+    clientZipCode: [''],
+    clientCountry: [''],
+    invoiceDate: [''],
+    paymentDueDate: [''],
+    paymentTerms: [''],
+    productDescription: [''],
+  });
 
-  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+  constructor(private fb: FormBuilder) {}
+
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(
+    event: KeyboardEvent
+  ) {
     this.cancelInvoice();
-    console.log('esc pressed')
+    console.log('esc pressed');
   }
-  
+
+  registerInvoice(): void {
+    console.log(this.form.value);
+  }
+
   cancelInvoice(): void {
     this.isInvoiceOpen = false;
     this.invoiceOpen.emit(this.isInvoiceOpen);
-    // clear form
   }
 }
